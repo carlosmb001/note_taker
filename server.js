f7fb95d2ce74e5f4aaeb7faf837c5c1b29339959
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-08-14 12:27:14 -0700

    added server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const routes = require('../fit_squad/controllers');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
