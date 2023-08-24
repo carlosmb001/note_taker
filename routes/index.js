dad808a6ff90589aad49936e71c83d2a68e55d9f
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-08-23 19:16:41 -0700

    completed

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,18 @@
-const express = require('express');
-const path = require('path');
+const router = require('express').Router();
 const fs = require('fs');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
-
-app.get('/api/notes', async (req, res) => {
-  try {
-    const notes = await fs.readFile('db.json', 'utf8');
+router.get('/api/notes', (req, res) => {
+  //try {
+    const notes = fs.readFileSync('./db/db.json', 'utf8');
     res.json(JSON.parse(notes));
-  } catch (error) {
+  /* } catch (error) {
     res.status(500).json({ error: 'Could not retrieve notes.' });
-  }
+  }*/
 });
 
-app.post('/api/notes', async (req, res) => {
+router.post('/api/notes', (req, res) => {
   try {
-    const notes = await fs.readFile('db.json', 'utf8');
+    const notes =  fs.readFileSync('./db/db.json', 'utf8');
     const notesArray = JSON.parse(notes);
 
     const newNote = {
@@ -38,7 +22,7 @@ app.post('/api/notes', async (req, res) => {
     };
 
     notesArray.push(newNote);
-    await fs.writeFile('db.json', JSON.stringify(notesArray));
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesArray));
 
     res.json(newNote);
   } catch (error) {
@@ -46,6 +30,4 @@ app.post('/api/notes', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -11,8 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+app.listen(PORT, () => console.log('Now listening'));
